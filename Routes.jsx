37be64f45cd744f67340ea055d38a0b376fe3e31
ebozpolat <162136417+ebozpolat@users.noreply.github.com>
2025-07-19 11:48:50 +0300

    Add files via upload

---
+++ Routes.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import ScrollToTop from "components/ScrollToTop";
+import ErrorBoundary from "components/ErrorBoundary";
+// Add your imports here
+import Login from "pages/login";
+import Register from "pages/register";
+import Dashboard from "pages/dashboard";
+import GlobalTradeSearch from "pages/global-trade-search";
+import NotFound from "pages/NotFound";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+      <ScrollToTop />
+      <RouterRoutes>
+        {/* Define your routes here */}
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/global-trade-search" element={<GlobalTradeSearch />} />
+        <Route path="*" element={<NotFound />} />
+      </RouterRoutes>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
